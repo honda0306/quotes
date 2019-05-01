9afc6ebd62555307ce7c2bfbbc1e25a4c316dbed
Chris Honda <christopher.honda92@gmail.com>
2019-05-01 13:00:23 -0700

    Express boilerplate and package installation

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.listen(port, () => {
+    console.log(`App is up, and is running on port ${port}`);
+});
